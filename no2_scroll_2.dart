31acc6f1c94f3ec6924d2902e39f90a10e1407cb
ProTaku <marshmallow4586@gmail.com>
2024-02-12 14:51:26 +0900

    [feat] No2. Scroll (2)
    - ScrollPhysics
    - ScrollController
    - ScrollConfiguration
    - Scrollbar
    - PageView
    - GridView

--- no2_scroll_2.dart
+++ no2_scroll_2.dart
@@ -9,11 +9,8 @@
 ///   - Scrollbar
 ///   - PageView
 ///   - GridView
-///   - NestedScrollView
-///   - "Linked Scroll Controller"
 ///
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ScrollPhysicsExameple extends StatelessWidget {
@@ -278,7 +275,7 @@ class _ScrollControllerExampleState extends State<ScrollControllerExample> {
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
+                  boxShadow: const [
                     BoxShadow(
                       color: Colors.grey,
                       offset: Offset(2, 2),
@@ -288,7 +285,7 @@ class _ScrollControllerExampleState extends State<ScrollControllerExample> {
                   ]
                 ),
                 alignment: Alignment.center,
-                child: Text("$index", style: TextStyle(
+                child: Text("$index", style: const TextStyle(
                   fontSize: 20, fontWeight: FontWeight.w500
                 ))
               );
@@ -298,7 +295,7 @@ class _ScrollControllerExampleState extends State<ScrollControllerExample> {
         Flexible(
           flex: 1,
           child: Center(child: Text(
-            offset ?? '', style: TextStyle(
+            offset ?? '', style: const TextStyle(
             fontSize: 18, fontWeight: FontWeight.w600
           )
           ))
@@ -316,11 +313,11 @@ class ScrollConfigurationExample extends StatelessWidget {
     return ScrollConfiguration(
       // behavior: CupertinoScrollBehavior(),
       // behavior: MaterialScrollBehavior(),
-      behavior: ScrollBehavior(),
+      behavior: const ScrollBehavior(),
       child: SingleChildScrollView(
         child: Container(
           height: 10000,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
@@ -360,11 +357,11 @@ class ScrollbarExample extends StatelessWidget {
                   child: Row(
                       mainAxisSize: MainAxisSize.max,
                       children: [
-                        Icon(Icons.person, size: 64, color: Colors.teal),
-                        SizedBox(width: 11),
+                        const Icon(Icons.person, size: 64, color: Colors.teal),
+                        const SizedBox(width: 11),
                         Text(
                           'No.$index card',
-                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                         )
                       ]
                   )
@@ -394,136 +391,136 @@ class PageViewExample extends StatelessWidget {
   }
 }
 
-// class GridViewExample extends StatelessWidget {
-//   const GridViewExample({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GridView.count(
-//
-//     );
-//   }
-// }
-
-class NestedScrollVieExample extends StatelessWidget {
-  const NestedScrollVieExample({super.key});
+class GridViewExample extends StatelessWidget {
+  const GridViewExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<String> tabs = <String>['Tab 1', 'Tab 2'];
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        // These are the slivers that show up in the "outer" scroll view.
-        return <Widget>[
-          SliverOverlapAbsorber(
-            // This widget takes the overlapping behavior of the SliverAppBar,
-            // and redirects it to the SliverOverlapInjector below. If it is
-            // missing, then it is possible for the nested "inner" scroll view
-            // below to end up under the SliverAppBar even when the inner
-            // scroll view thinks it has not been scrolled.
-            // This is not necessary if the "headerSliverBuilder" only builds
-            // widgets that do not overlap the next sliver.
-            handle:
-            NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-            sliver: SliverAppBar(
-              title:
-              const Text('Books'), // This is the title in the app bar.
-              pinned: true,
-              expandedHeight: 150.0,
-              // The "forceElevated" property causes the SliverAppBar to show
-              // a shadow. The "innerBoxIsScrolled" parameter is true when the
-              // inner scroll view is scrolled beyond its "zero" point, i.e.
-              // when it appears to be scrolled below the SliverAppBar.
-              // Without this, there are cases where the shadow would appear
-              // or not appear inappropriately, because the SliverAppBar is
-              // not actually aware of the precise position of the inner
-              // scroll views.
-              forceElevated: innerBoxIsScrolled,
-              bottom: TabBar(
-                // These are the widgets to put in each tab in the tab bar.
-                tabs: tabs.map((String name) => Tab(text: name)).toList(),
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: PageView(
+        children: [
+          /// GridView
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: Text('GridView', style: TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.bold
+                )),
               ),
-            ),
+              Expanded(
+                child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 5.0,
+                    crossAxisSpacing: 10.0
+                  ),
+                  children: List.generate(100, (index) {
+                    late Color color;
+                    late IconData icon;
+
+                    if (index.isEven) {
+                      color = Colors.blueAccent;
+                      icon = Icons.thumb_up;
+                    } else {
+                      color = Colors.lightBlue;
+                      icon = Icons.thumb_down;
+                    }
+
+                    return Container(
+                      color: color,
+                      alignment: Alignment.center,
+                      child: Icon(icon, size: 28)
+                    );
+                  }),
+                ),
+              ),
+            ],
           ),
-        ];
-      },
-      body: TabBarView(
-        // These are the contents of the tab views, below the tabs.
-        children: tabs.map((String name) {
-          return SafeArea(
-            top: false,
-            bottom: false,
-            child: Builder(
-              // This Builder is needed to provide a BuildContext that is
-              // "inside" the NestedScrollView, so that
-              // sliverOverlapAbsorberHandleFor() can find the
-              // NestedScrollView.
-              builder: (BuildContext context) {
-                return CustomScrollView(
-                  // The "controller" and "primary" members should be left
-                  // unset, so that the NestedScrollView can control this
-                  // inner scroll view.
-                  // If the "controller" property is set, then this scroll
-                  // view will not be associated with the NestedScrollView.
-                  // The PageStorageKey should be unique to this ScrollView;
-                  // it allows the list to remember its scroll position when
-                  // the tab view is not on the screen.
-                  key: PageStorageKey<String>(name),
-                  slivers: <Widget>[
-                    SliverOverlapInjector(
-                      // This is the flip side of the SliverOverlapAbsorber
-                      // above.
-                      handle:
-                      NestedScrollView.sliverOverlapAbsorberHandleFor(
-                          context),
-                    ),
-                    SliverPadding(
-                      padding: const EdgeInsets.all(8.0),
-                      // In this example, the inner scroll view has
-                      // fixed-height list items, hence the use of
-                      // SliverFixedExtentList. However, one could use any
-                      // sliver widget here, e.g. SliverList or SliverGrid.
-                      sliver: SliverFixedExtentList(
-                        // The items in this example are fixed to 48 pixels
-                        // high. This matches the Material Design spec for
-                        // ListTile widgets.
-                        itemExtent: 48.0,
-                        delegate: SliverChildBuilderDelegate(
-                              (BuildContext context, int index) {
-                            // This builder is called for each child.
-                            // In this example, we just number each list item.
-                            return ListTile(
-                              title: Text('Item $index'),
-                            );
-                          },
-                          // The childCount of the SliverChildBuilderDelegate
-                          // specifies how many children this inner list
-                          // has. In this example, each tab has a list of
-                          // exactly 30 items, but this is arbitrary.
-                          childCount: 30,
-                        ),
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          );
-        }).toList(),
+
+          /// GridView.builder
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: Text('GridView.builder', style: TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.bold
+                ))
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 20,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 5.0,
+                      crossAxisSpacing: 10.0
+                  ),
+                  itemBuilder: (context, index) {
+                    late Color color;
+                    late IconData icon;
+
+                    if (index.isEven) {
+                      color = Colors.blueAccent;
+                      icon = Icons.thumb_up;
+                    } else {
+                      color = Colors.lightBlue;
+                      icon = Icons.thumb_down;
+                    }
+
+                    return Container(
+                        color: color,
+                        alignment: Alignment.center,
+                        child: Icon(icon, size: 56)
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          /// GridView.count
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: Text('GridView.count', style: TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.bold
+                )),
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 10,
+                  children: List.generate(99, (index) {
+                    late Color color;
+                    late IconData icon;
+
+                    if (index.isEven) {
+                      color = Colors.blueAccent;
+                      icon = Icons.thumb_up;
+                    } else {
+                      color = Colors.lightBlue;
+                      icon = Icons.thumb_down;
+                    }
+
+                    return Container(
+                        color: color,
+                        alignment: Alignment.center,
+                        child: Icon(icon, size: 28)
+                    );
+                  })
+                ),
+              ),
+            ],
+          ),
+        ]
       ),
     );
-  }
-}
-
-class LinkedScrollControllerExample extends StatefulWidget {
-  const LinkedScrollControllerExample({super.key});
-
-  @override
-  State<LinkedScrollControllerExample> createState() => _LinkedScrollControllerExampleState();
-}
-class _LinkedScrollControllerExampleState extends State<LinkedScrollControllerExample> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
